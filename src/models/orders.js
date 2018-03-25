63aa6a27a4f141bc62d9e9d475e8c565e86de91c
Mariia Polova <maria.polevaja@gmail.com>
2018-03-25 11:32:38 +0300

    update of models

--- src/models/orders.js
+++ src/models/orders.js
@@ -6,7 +6,8 @@ const schema = new Schema({
     id: { type: Number, required: true },
     created: { type:Date, default: Date.now },
     customer_id: { type: String, required: true },
-    products: [{ product_id: String, quantity: Number }],
+    customer_name: { type: String, required: true },
+    products: [{ product_id: String, product_title: String, product_price: Number, quantity: Number }],
     status: {type: String, required: true, enum: ['Open', 'Processed', 'Failed', 'Cancelled'], default: 'Open'}
 });
 
--- src/models/products.js
+++ src/models/products.js
@@ -3,19 +3,18 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    title: { type: String, required: true },
-    price: { type: Number, required: true },
-    created: { type:Date, default: Date.now }, // diff new field
-    description: { type: String, required: true },
-    category: { type: String }, // can be array of categories
-    color: { type: String },
-    rating: { type: Number, min: 0, max: 5 },
-    img: [String],
-    accessible: { type: Boolean, default: true },
-    preset: { type: String },
-    producer: { type: String },
-    memory: { type: String },
-    capacity: { type: Number }
+        title: { type: String, required: true },
+        description: { type: String, required: true },
+        category: { type: String, enum: ['Desktops', 'Laptops', 'Tablets', 'Phones'] },
+        price: { type: Number },
+        color: { type: String, enum: ['Silver', 'Gold', 'Rose', 'Black'] },
+        rating_value: { type: Number, default: 0 },
+        rating_votes: { type: Number, default: 0 },
+        img: [String],
+        amount: { type: Number, default: 0 },
+        filters_memory: {type: String},
+        filters_ram: {type: String},
+        filters_producer: {type: String}
 });
 
 export default mongoose.model('products', schema);
